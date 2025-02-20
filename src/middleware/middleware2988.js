01b2d13a79f3a52c4c1cc6b07b791315ed3f264d
shivas1432 <shivashanker7337@gmail.com>
2025-02-20 18:05:54 +0000

    Fix security vulnerability

---
+++ src/middleware/middleware2988.js
@@ -0,0 +1,20 @@
+﻿// Middleware 2988 - Socialmedia_react_BE
+const middleware2988 = (req, res, next) => {
+  try {
+    // Add middleware logic
+    req.middlewareId = 2988;
+    req.processedAt = new Date();
+    
+    // Log request
+    console.log('Middleware 2988 processed request:', req.url);
+    
+    next();
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      error: 'Middleware error: ' + error.message
+    });
+  }
+};
+
+module.exports = middleware2988;
